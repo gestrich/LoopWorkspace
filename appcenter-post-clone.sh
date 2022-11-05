4617e70874dab69bb120a00c8193ebefbcc6567d
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2022-11-05 18:43:14 -0400

    Add provisioning profiles

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -23,3 +23,10 @@ echo "https://gestrich:$GIT_TOKEN@github.com" > ~/.git-credentials
 echo "[credential]" >> ~/.gitconfig
 echo "  username = gestrich" >> ~/.gitconfig
 echo "  helper = store" >> ~/.gitconfig
+
+#Copy provisioning profiles
+mv profiles/1887b257-3f9b-4223-89a3-8f98fddd8ea1.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
+mv profiles/6d1a531b-fca9-4485-81a4-b54adb90bb69.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
+mv profiles/99accffc-a353-425f-baf3-15884221b680.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
+mv profiles/f41cd326-5705-42f4-90f6-1e3d9228adc8.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
+ls ~/Library/MobileDevice/Provisioning\ Profiles
